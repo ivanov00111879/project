518149c396fb5d7ba9fee9631592657b635e5daa
Valeriy G. <it@pochta.com>
2026-05-14 15:35:14 +0200

    kibana

---
+++ logging/set-kibana.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Пути к файлам
+SOURCE="/home/debian/project/logging/kibana.yml"
+DEST="/etc/kibana/kibana.yml"
+
+# Проверка на права root
+if [ "$EUID" -ne 0 ]; then 
+  echo "Пожалуйста, запустите скрипт с sudo"
+  exit 1
+fi
+
+# 1. Копирование файла
+if [ -f "$SOURCE" ]; then
+    cp "$SOURCE" "$DEST"
+    echo "Configuration completed"
+else
+    echo "Ошибка: Исходный файл $SOURCE не найден"
+    exit 1
+fi
+
+# 2. Перезапуск службы
+    systemctl restart kibana
+    echo "Kibana успешно перезапущена"
