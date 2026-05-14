61e60e04ce4cd63e8f98d704ed55abac117987dc
Valeriy G. <it@pochta.com>
2026-05-14 20:26:59 +0200

    settings

---
+++ logging/set-logging.sh
@@ -0,0 +1,4 @@
+sudo bash /home/debian/project/logging/set-elastic.sh
+sudo bash /home/debian/project/logging/set-kibana.sh
+sudo bash /home/debian/project/logging/set-logstash.sh
+sudo bash /home/debian/project/logging/set-logstash-nginx.sh
