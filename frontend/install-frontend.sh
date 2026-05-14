7537408ceb11a9ded39ebc1a6ca80031c46cfad8
Valeriy G. <it@pochta.com>
2026-05-14 20:06:44 +0200

    install

---
+++ frontend/install-frontend.sh
@@ -0,0 +1,3 @@
+sudo bash /home/debian/project/frontend/install-nginx.sh
+sudo bash /home/debian/project/monitoring/install-prometheus-node-exporther.sh
+sudo bash /home/debian/project/frontend/install-filebeat.sh
